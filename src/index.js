0ea90754d8e41a37b0d51035d676e7fef453829f
KindaExists <ehrencastillo@gmail.com>
2021-12-11 16:18:15 +0800

    add factories

---
+++ src/index.js
@@ -0,0 +1,51 @@
+
+const Ship = (length) => {
+  const rets = {
+    length,
+    hit() {
+      // Hit Code Here
+    },
+    isSunk() {
+      // Check if Ship is Sunken code here
+
+    }
+  };
+
+  return rets;
+}
+
+const Cell = (pos) => {
+  return{
+    pos
+  };
+};
+
+const Gameboard = (size) => {
+  return{
+    size
+  };
+};
+
+const Player = (name) => {
+  return {
+    name
+  };
+};
+
+const display = (() => {
+  return {
+
+  };
+})();
+
+
+function gameLoop() {
+
+}
+
+
+function start() {
+
+}
+
+start();
