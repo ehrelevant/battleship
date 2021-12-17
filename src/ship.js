e953624366bf40aad2d4de495a247c67cfd18fde
KindaExists <ehrencastillo@gmail.com>
2021-12-17 16:59:57 +0800

    made position and orientation parameters

--- src/ship.js
+++ src/ship.js
@@ -1,18 +1,16 @@
-const Ship = (length) => {
+const Ship = (length, pos, orientation) => {
+  const cells = Array(length).fill(true);
+  
   const rets = {
-    cells: Array(length).fill(true),
-    pos: null,
-    rotation: null,
+    cells,
+    pos,
+    orientation,
     hit(cellIndex) {
       this.cells[cellIndex] = false;
     },
     isSunk() {
       return rets.cells.every(cell => (cell == false));
     },
-    setPos(headPos, rotation) {
-      this.pos = headPos;
-      this.rotation = rotation;
-    }
   };
 
   return rets;
--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,7 +1,7 @@
 import Ship from '../ship.js';
 
 test('Ship has cells equal to length', () => {
-  const ship = Ship(4);
+  const ship = Ship(4, {x: 0, y: 0}, 'horizontal');
 
   expect(ship).toMatchObject({
     cells: [true, true, true, true]
@@ -9,7 +9,7 @@ test('Ship has cells equal to length', () => {
 });
 
 test('Hit() sets a ship cell to false', () => {
-  const ship = Ship(5);
+  const ship = Ship(5, {x: 0, y: 0}, 'horizontal');
   ship.hit(0);
   ship.hit(2);
 
@@ -19,11 +19,20 @@ test('Hit() sets a ship cell to false', () => {
 });
 
 test('Checks if ship has been sunken', () => {
-  const ship = Ship(3);
+  const ship = Ship(3, {x: 0, y: 0}, 'horizontal');
   ship.hit(0);
   ship.hit(1);
   ship.hit(2);
 
   expect(ship.isSunk()).toEqual(true);
+});
+
+test('Ship has a position and rotation', () => {
+  const ship = Ship(5, {x: 3, y: 4}, 'vertical');
+
+  expect(ship).toMatchObject({
+    pos: {x: 3, y: 4},
+    orientation: 'vertical'
+  });
 });
 
