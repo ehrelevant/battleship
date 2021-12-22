ac6312a89b4d94e20a5184ae27d7d9c7ea61a68a
KindaExists <ehrencastillo@gmail.com>
2021-12-22 17:54:37 +0800

    add basic foundation for gameloop

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import Player from "./gameObjects/player";
 
 
 function start() {
+  const player = Player('One')
+  const computerAI = Player('CPU', true)
 
+  const players = [player, computerAI]
+
+  game(players)
 }
 
 function game(players) {
-  while(true) {
+  let turn = 0;
+  const playerCount = players.length;
 
+  while(true) {
+    const currentPlayer = players[(turn % playerCount)];
   }
 }
 
